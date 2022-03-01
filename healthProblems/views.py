98e00e2079231b312cd46208a2a80888d7776dec
geogiadim <gia1914g@gmail.com>
2022-03-01 05:12:44 +0200

    charts-almost done

--- healthProblems/views.py
+++ healthProblems/views.py
@@ -10,7 +10,6 @@ def index(request):
 def ChartRenderView(request):
     # get the url params from axios request
     patient = request.GET.get('userVal')
-    selected_problems = request.GET.get('probVal')
     start_date_range = request.GET.get('startDate')
     end_date_range = request.GET.get('endDate')
 
@@ -24,37 +23,39 @@ def ChartRenderView(request):
     list_pts = list(pts.split(","))
     list_dates = list(dates.split(","))
 
-    # print(patient)
-    # print(selected_problems)
-    # print(start_date_range)
-    # print(end_date_range)
-    # print(list_patient_problems)
-    # print(list_rates)
-    # print(list_pts)
-    # print(list_dates)
-
     df = pd.DataFrame()
     df['patient'] = list_pts
     df['problem'] = list_patient_problems
     df['rates'] = list_rates
     df['dates'] = list_dates
-    new_df = df[df.patient == patient]
-    new_df = new_df[new_df.dates >= start_date_range]
-    new_df = new_df[new_df.dates <= end_date_range]
-    print()
-    print(new_df)
-    print()
+    clean_df = df[df.patient == patient]
+    clean_df = clean_df[clean_df.dates >= start_date_range]
+    clean_df = clean_df[clean_df.dates <= end_date_range]
+
+    unique_dates = set(clean_df['dates'])
+    list_unique_dates = list(unique_dates)
+    list_unique_dates.sort()
+
+    sleep_df = clean_df[clean_df.problem == 'TooMuchSleepProblem']
+    sleep_df = sleep_df.append(clean_df[clean_df.problem == 'LackOfSleepProblem'])
+    sleep_df = sleep_df.append(clean_df[clean_df.problem == 'LowSleepQualityProblem'])
+    hr_df = clean_df[clean_df.problem == 'LowHeartRateProblem']
+    movement_df = clean_df[clean_df.problem == 'LackOfMovementProblem']
     # context
     context = {'data': False, 'model': request.GET.get('model')}
+    print("Unique dates: ", list_unique_dates)
     if request.GET.get('model') == 'sleep-problems':
+        print(sleep_df)
         context['model_name'] = 'Sleep Problems'
         context['data'] = True
         return render(request, "healthProblems/_chart.html", context)
     if request.GET.get('model') == 'heartRate-problems':
+        print(hr_df)
         context['model_name'] = 'Heart Rate Problems'
         context['data'] = True
         return render(request, "healthProblems/_chart.html", context)
     if request.GET.get('model') == 'movement-problems':
+        print(movement_df)
         context['model_name'] = 'Movement Problems'
         context['data'] = True
         return render(request, "healthProblems/_chart.html", context)
